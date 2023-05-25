412b372b0c4783ec5dc6e122e749a72dc1b4cabb
Donald Ebube <donaldebube@yahoo.com>
2023-05-25 16:43:29 +0100

    New Commit

--- Movies Data Comparison.sql
+++ Movies Data Comparison.sql
@@ -15,3 +15,29 @@ SELECT movie_title, rating
 FROM naija_movies
 ORDER BY rating DESC
 GO
+
+SELECT *
+FROM naija_movies
+GO
+
+SELECT *
+FROM Hydra_Movie_Scrape_1
+ORDER BY [Year] DESC
+GO
+
+SELECT DISTINCT [Year]
+FROM Hydra_Movie_Scrape_1
+GO
+
+SELECT DISTINCT [year]
+FROM naija_movies
+ORDER BY [year] DESC
+GO
+
+SELECT *
+FROM naija_movies
+GO
+
+SELECT *
+FROM Hydra_Movie_Scrape_1
+GO
