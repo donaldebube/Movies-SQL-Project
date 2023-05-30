98c29e8f4c2daa1bc532bf403c770e4c67ed4b02
donaldebube <donaldebube@yahoo.com>
2023-05-30 11:10:44 +0100

    New Commit

--- Movies_Data_Practice.sql
+++ Movies_Data_Practice.sql
@@ -24,3 +24,53 @@ GO
 SELECT COUNT(*) AS [Foereign Movies]
 FROM foreign_movies_set
 GO
+
+SELECT *
+FROM naija_movies_set
+GO
+
+
+-- ALTER TABLE naija_movies_set
+-- RENAME COLUMN index TO S/N
+-- GO
+
+-- Change of column names
+-- For index column
+EXEC sp_rename 'naija_movies_set.index', 'S/N', 'COLUMN'
+GO
+
+-- For title column
+EXEC sp_rename 'naija_movies_set.title', 'Title', 'COLUMN'
+GO
+
+-- For year column
+EXEC sp_rename 'naija_movies_set.year', 'Year', 'COLUMN'
+GO
+
+-- For genre column
+EXEC sp_rename 'naija_movies_set.genre', 'Genre', 'COLUMN'
+GO
+
+-- For certificate column
+EXEC sp_rename 'naija_movies_set.certificate', 'Certificate', 'COLUMN'
+GO
+
+-- For rating column
+EXEC sp_rename 'naija_movies_set.rating', 'Rating', 'COLUMN'
+GO
+
+-- For directors column
+EXEC sp_rename 'naija_movies_set.directors', 'Directors', 'COLUMN'
+GO
+
+-- For synopsis column
+EXEC sp_rename 'naija_movies_set.synopsis', 'Synopsis', 'COLUMN'
+GO
+
+-- For cast column
+EXEC sp_rename 'naija_movies_set.cast', 'Cast', 'COLUMN'
+GO
+
+-- Drop movie_title column
+
+
