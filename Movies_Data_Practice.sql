845ad82d2e533ed16d7c7e52b1dccf01246050f5
donaldebube <donaldebube@yahoo.com>
2023-06-05 10:58:48 +0100

    New Commit

--- Movies_Data_Practice.sql
+++ Movies_Data_Practice.sql
@@ -35,297 +35,302 @@ GO
 
 -- Cleaning the Nigerian Local Dataset
 
--- Change of column names
--- For index column
-EXEC sp_rename 'naija_movies_set.index', 'S/N', 'COLUMN'
-GO
+  -- Change of column names
+    -- For index column
 
--- For title column
-EXEC sp_rename 'naija_movies_set.title', 'Title', 'COLUMN'
-GO
+    EXEC sp_rename 'naija_movies_set.index', 'S/N', 'COLUMN'
+    GO
 
--- For year column
-EXEC sp_rename 'naija_movies_set.year', 'Year', 'COLUMN'
-GO
+    -- For title column
+    EXEC sp_rename 'naija_movies_set.title', 'Title', 'COLUMN'
+    GO
 
--- For genre column
-EXEC sp_rename 'naija_movies_set.genre', 'Genre', 'COLUMN'
-GO
+    -- For year column
+    EXEC sp_rename 'naija_movies_set.year', 'Year', 'COLUMN'
+    GO
 
--- For certificate column
-EXEC sp_rename 'naija_movies_set.certificate', 'Certificate', 'COLUMN'
-GO
+    -- For genre column
+    EXEC sp_rename 'naija_movies_set.genre', 'Genre', 'COLUMN'
+    GO
 
--- For rating column
-EXEC sp_rename 'naija_movies_set.rating', 'Rating', 'COLUMN'
-GO
+    -- For certificate column
+    EXEC sp_rename 'naija_movies_set.certificate', 'Certificate', 'COLUMN'
+    GO
 
--- For directors column
-EXEC sp_rename 'naija_movies_set.directors', 'Directors', 'COLUMN'
-GO
+    -- For rating column
+    EXEC sp_rename 'naija_movies_set.rating', 'Rating', 'COLUMN'
+    GO
 
--- For synopsis column
-EXEC sp_rename 'naija_movies_set.synopsis', 'Synopsis', 'COLUMN'
-GO
+    -- For directors column
+    EXEC sp_rename 'naija_movies_set.directors', 'Directors', 'COLUMN'
+    GO
 
--- For cast column
-EXEC sp_rename 'naija_movies_set.cast', 'Cast', 'COLUMN'
-GO
+    -- For synopsis column
+    EXEC sp_rename 'naija_movies_set.synopsis', 'Synopsis', 'COLUMN'
+    GO
 
--- Drop movie_title column
-ALTER TABLE naija_movies_set
-DROP COLUMN movie_title
-GO
+    -- For cast column
+    EXEC sp_rename 'naija_movies_set.cast', 'Cast', 'COLUMN'
+    GO
 
--- Check for NULL valiues in the different columns
--- Cleaning the title column
-SELECT DISTINCT title
-FROM naija_movies_set
-WHERE Title LIKE '%D'
-GO
+  
+  
+  -- For Movie Title column
+    -- Drop movie_title column
+    ALTER TABLE naija_movies_set
+    DROP COLUMN movie_title
+  GO
 
--- For Year
+  
+  -- For Title Column
+    SELECT DISTINCT title
+    FROM naija_movies_set
+    WHERE Title LIKE '%D'
+  GO
 
--- Checking the different unique values in the Year column
-SELECT DISTINCT YEAR
-FROM naija_movies_set
-GO
+  -- For Year
 
--- For rows with empty column (COUNT)
-SELECT DISTINCT [year], COUNT([year])
-FROM naija_movies_set
-WHERE [year] = ''
-GROUP BY [year]
-GO
+    -- Checking the different unique values in the Year column
+    SELECT DISTINCT YEAR
+    FROM naija_movies_set
+    GO
 
--- For rows with empty column
-SELECT [year], title
-FROM naija_movies_set
-WHERE [year] = ''
-GROUP BY [year], title
-GO
+    -- For rows with empty column (COUNT)
+    SELECT DISTINCT [year], COUNT([year])
+    FROM naija_movies_set
+    WHERE [year] = ''
+    GROUP BY [year]
+    GO
 
--- For rows with 'Video' at the end of the row 
-SELECT [year], title
-FROM naija_movies_set
-WHERE [year] LIKE '%Video'
-GROUP BY [year], title
-GO
+    -- For rows with empty column
+    SELECT [year], title
+    FROM naija_movies_set
+    WHERE [year] = ''
+    GROUP BY [year], title
+    GO
 
--- For rows with 'Video' at the end of the row (COUNT)
-SELECT COUNT(*)
-FROM naija_movies_set
-WHERE [year] LIKE '%Video'
-GO
+    -- For rows with 'Video' at the end of the row 
+    SELECT [year], title
+    FROM naija_movies_set
+    WHERE [year] LIKE '%Video'
+    GROUP BY [year], title
+    GO
 
--- Replace the empty columns with Not Provided
-UPDATE naija_movies_set
-SET [year] = 'Not Provided'
-WHERE year = ''
-GO
+    -- For rows with 'Video' at the end of the row (COUNT)
+    SELECT COUNT(*)
+    FROM naija_movies_set
+    WHERE [year] LIKE '%Video'
+    GO
 
--- Replace the columns that have video at the end with an empty space
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, ' Video', '')
-WHERE [year] LIKE '% Video';
-GO
+    -- Replace the empty columns with Not Provided
+    UPDATE naija_movies_set
+    SET [year] = 'Not Provided'
+    WHERE year = ''
+    GO
 
--- Replace the columns that have TV Movie at the end with an empty space
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, ' TV Movie', '')
-WHERE [year] LIKE '% TV Movie';
-GO
+    -- Replace the columns that have video at the end with an empty space
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, ' Video', '')
+    WHERE [year] LIKE '% Video';
+    GO
 
--- Replace the columns that have TV Special at the end with an empty space
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, ' TV Special', '')
-WHERE [year] LIKE '% TV Special';
-GO
+    -- Replace the columns that have TV Movie at the end with an empty space
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, ' TV Movie', '')
+    WHERE [year] LIKE '% TV Movie';
+    GO
 
--- Replace the columns that have TV Short at the end with an empty space
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, ' TV Short', '')
-WHERE [year] LIKE '% TV Short';
-GO
+    -- Replace the columns that have TV Special at the end with an empty space
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, ' TV Special', '')
+    WHERE [year] LIKE '% TV Special';
+    GO
 
--- Replace the columns that have Music at the end with an empty space
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, ' Music', '')
-WHERE [year] LIKE '% Music';
-GO
+    -- Replace the columns that have TV Short at the end with an empty space
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, ' TV Short', '')
+    WHERE [year] LIKE '% TV Short';
+    GO
 
--- Check I COLUMN
-SELECT COUNT(*)
-FROM naija_movies_set
-WHERE [year] = 'I'
-GO
+    -- Replace the columns that have Music at the end with an empty space
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, ' Music', '')
+    WHERE [year] LIKE '% Music';
+    GO
 
--- Update 
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, 'I', 'Not Provided')
-WHERE [year] = 'I'
-GO
+    -- Check I COLUMN
+    SELECT COUNT(*)
+    FROM naija_movies_set
+    WHERE [year] = 'I'
+    GO
 
--- Update 
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, 'V', 'Not Provided')
-WHERE [year] = 'V'
-GO
+    -- Update 
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, 'I', 'Not Provided')
+    WHERE [year] = 'I'
+    GO
 
--- Update 
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, 'II', 'Not Provided')
-WHERE [year] = 'II'
-GO
+    -- Update 
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, 'V', 'Not Provided')
+    WHERE [year] = 'V'
+    GO
 
--- Update 
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, 'IX', 'Not Provided')
-WHERE [year] = 'IX'
-GO
+    -- Update 
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, 'II', 'Not Provided')
+    WHERE [year] = 'II'
+    GO
 
--- Update 
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, 'deo', 'Not Provided')
-WHERE [year] = 'deo'
-GO
+    -- Update 
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, 'IX', 'Not Provided')
+    WHERE [year] = 'IX'
+    GO
 
--- Update 
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, '', 'Not Provided')
-WHERE [year] = ' %'
-GO
+    -- Update 
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, 'deo', 'Not Provided')
+    WHERE [year] = 'deo'
+    GO
 
--- Update 
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, 'VI', '')
-WHERE [year] LIKE 'VI%'
-GO
+    -- Update 
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, '', 'Not Provided')
+    WHERE [year] = ' %'
+    GO
 
--- Update 
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, 'III', '')
-WHERE [year] LIKE 'III%'
-GO
+    -- Update 
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, 'VI', '')
+    WHERE [year] LIKE 'VI%'
+    GO
 
--- Update 
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, 'II', '')
-WHERE [year] LIKE 'II%'
-GO
+    -- Update 
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, 'III', '')
+    WHERE [year] LIKE 'III%'
+    GO
 
--- Update 
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, 'II ', '')
-WHERE [year] LIKE 'II%'
-GO
+    -- Update 
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, 'II', '')
+    WHERE [year] LIKE 'II%'
+    GO
 
--- Update 
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, 'I', '')
-WHERE [year] LIKE 'I%'
-GO
+    -- Update 
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, 'II ', '')
+    WHERE [year] LIKE 'II%'
+    GO
 
--- Update 
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, 'X', '')
-WHERE [year] LIKE 'X%'
-GO
+    -- Update 
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, 'I', '')
+    WHERE [year] LIKE 'I%'
+    GO
 
--- Update 
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, '-', '!')
-WHERE [year] LIKE '%-%'
-GO
+    -- Update 
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, 'X', '')
+    WHERE [year] LIKE 'X%'
+    GO
 
--- Update 
-UPDATE naija_movies_set
-SET [year] = REPLACE(year, '2011-', '2011')
-WHERE [year] LIKE '2011-%'
-GO
+    -- Update 
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, '-', '!')
+    WHERE [year] LIKE '%-%'
+    GO
 
--- Verify
-SELECT *
-FROM naija_movies_set
-WHERE year LIKE 'VI%'
-GO
+    -- Update 
+    UPDATE naija_movies_set
+    SET [year] = REPLACE(year, '2011-', '2011')
+    WHERE [year] LIKE '2011-%'
+    GO
 
--- Verify
-SELECT *
-FROM naija_movies_set
-WHERE year LIKE 'VI%'
-GO
+    -- Verify
+    SELECT *
+    FROM naija_movies_set
+    WHERE year LIKE 'VI%'
+    GO
 
--- Verify
-SELECT *
-FROM naija_movies_set
-GO
+    -- Verify
+    SELECT *
+    FROM naija_movies_set
+    WHERE year LIKE 'VI%'
+    GO
 
--- 425
-SELECT COUNT(*)
-FROM naija_movies_set
-WHERE YEAR = 'deo'
-GO
+    -- Verify
+    SELECT *
+    FROM naija_movies_set
+    GO
 
-SELECT *
-FROM naija_movies_set
-WHERE [year] = 'deo'
-GO
+    -- 425
+    SELECT COUNT(*)
+    FROM naija_movies_set
+    WHERE YEAR = 'deo'
+    GO
 
-UPDATE naija_movies_set
-SET [year] = CONCAT([year], 'Present')
-WHERE [year] LIKE '%-%';
+    SELECT *
+    FROM naija_movies_set
+    WHERE [year] = 'deo'
+    GO
 
-SELECT DISTINCT [year]
-FROM naija_movies_set
-GO
+    UPDATE naija_movies_set
+    SET [year] = CONCAT([year], 'Present')
+    WHERE [year] LIKE '%-%';
+
+    SELECT DISTINCT [year]
+    FROM naija_movies_set
+    GO
 
 
--- To remove the empty spaces in the column
-UPDATE naija_movies_set
-SET [year] = RTRIM([year])
-WHERE [year] LIKE '% '
-GO
+    -- To remove the empty spaces in the column
+    UPDATE naija_movies_set
+    SET [year] = RTRIM([year])
+    WHERE [year] LIKE '% '
+    GO
 
--- UPDATE naija_movies_set
--- SET [year] = TRIM(REPLACE([year], '-', ''))
--- WHERE [year] LIKE '%-%'
+    -- UPDATE naija_movies_set
+    -- SET [year] = TRIM(REPLACE([year], '-', ''))
+    -- WHERE [year] LIKE '%-%'
+    -- GO
+
+    -- UPDATE naija_movies_set
+    -- SET [year] = CASE
+    --     WHEN RIGHT([year], 1) = '-' THEN LEFT([year], LEN([year]) - 1)
+    --     ELSE [year]
+    -- END
+    -- WHERE [year] LIKE '%-'
+  GO
+  
+  -- For Certificate
+    -- Drop CERTIFICATE column
+    ALTER TABLE naija_movies_set
+    DROP COLUMN certificate
+  GO
+
+  -- For Rating
+    SELECT *
+    FROM naija_movies_set
+    WHERE Rating IS NULL
+    -- GROUP BY Rating
+    GO
+
+    -- Drop Rating column
+    ALTER TABLE naija_movies_set
+    DROP COLUMN Rating
+    GO
+  GO
+
+  -- For Synopsis
+  SELECT Synopsis, COUNT(*)
+  FROM naija_movies_set
+  --WHERE Synopsis IS NULL
+  GROUP BY  Synopsis
+  GO
+
 -- GO
-
--- UPDATE naija_movies_set
--- SET [year] = CASE
---     WHEN RIGHT([year], 1) = '-' THEN LEFT([year], LEN([year]) - 1)
---     ELSE [year]
--- END
--- WHERE [year] LIKE '%-'
-
--- Drop CERTIFICATE column
-ALTER TABLE naija_movies_set
-DROP COLUMN certificate
-GO
-
-SELECT *
-FROM naija_movies_set
-GO
-
--- For Rating
-SELECT *
-FROM naija_movies_set
-WHERE Rating IS NULL
--- GROUP BY Rating
-GO
-
--- Drop Rating column
-ALTER TABLE naija_movies_set
-DROP COLUMN Rating
-GO
-
--- For Synopsis
-SELECT Synopsis, COUNT(*)
-FROM naija_movies_set
---WHERE Synopsis IS NULL
-GROUP BY  Synopsis
-GO
 
 -- Analysis Proper
 
@@ -336,5 +341,9 @@ FROM naija_movies_set
 GROUP BY YEAR
 ORDER BY [Number of Movies Released] DESC
 GO
+
+  SELECT *
+  FROM naija_movies_set
+  GO
 
 
